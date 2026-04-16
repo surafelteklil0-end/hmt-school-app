9b47701c272f53da93f731624a691e0dbd8e1915
surafelteklil0-end <surafelteklil0@gmail.com>
2026-04-16 18:51:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const langData = {
+  am: "እንኳን ወደ H.M.T ተቀባችሁ",
+  en: "Welcome to H.M.T School"
+};
 
+function setLang(lang){
+  localStorage.setItem("lang", lang);
+  document.getElementById("text").innerText = langData[lang];
+}
+
+window.onload = function(){
+  let lang = localStorage.getItem("lang") || "am";
+  document.getElementById("text").innerText = langData[lang];
+};
