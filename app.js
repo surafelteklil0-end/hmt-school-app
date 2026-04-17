27940cb2ec483384985a5ac118aaefad03e2a481
surafelteklil0-end <surafelteklil0@gmail.com>
2026-04-18 00:44:48 +0300

    Update app.js
    Signed-off-by: surafelteklil0-end <surafelteklil0@gmail.com>

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-function login() {
-  let user = document.getElementById("username").value;
-  let pass = document.getElementById("password").value;
-  if (user === "admin" && pass === "1234") {
-    localStorage.setItem("loggedIn", "true");
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Wrong username or password");
-  }
-}
-function logout() {
-  localStorage.removeItem("loggedIn");
-  window.location.href = "index.html";
-}
+0
