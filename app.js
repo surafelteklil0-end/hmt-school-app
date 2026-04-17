12c0e7e9b2c5c6811fc426ef2befdcbfbd1f1c61
surafelteklil0-end <surafelteklil0@gmail.com>
2026-04-17 07:22:51 +0300

    Update app.js
    Signed-off-by: surafelteklil0-end <surafelteklil0@gmail.com>

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-const langData = {
-  am: "እንኳን ወደ H.M.T ተቀባችሁ",
-  en: "Welcome to H.M.T School"
+// language
+const lang = {
+  am: {
+    title: "መግቢያ",
+    login: "ግባ",
+    success: "በትክክል ገብተዋል",
+    fail: "ስህተት ነው"
+  },
+  en: {
+    title: "Login",
+    login: "Login",
+    success: "Login success",
+    fail: "Wrong username or password"
+  }
 };
 
-function setLang(lang){
-  localStorage.setItem("lang", lang);
-  document.getElementById("text").innerText = langData[lang];
+// default user (temporary)
+const users = [
+  {username:"owner", password:"1234", role:"owner"},
+  {username:"admin", password:"1234", role:"admin"}
+];
+
+function login(){
+  let u = document.getElementById("username").value;
+  let p = document.getElementById("password").value;
+
+  let user = users.find(x => x.username === u && x.password === p);
+
+  let currentLang = localStorage.getItem("lang") || "en";
+
+  if(user){
+    document.getElementById("msg").innerText = lang[currentLang].success;
+  }else{
+    document.getElementById("msg").innerText = lang[currentLang].fail;
+  }
 }
 
-window.onload = function(){
-  let lang = localStorage.getItem("lang") || "am";
-  document.getElementById("text").innerText = langData[lang];
-};
+function setLang(l){
+  localStorage.setItem("lang", l);
+  applyLang();
+}
+
+function applyLang(){
+  let l = localStorage.getItem("lang") || "en";
+  document.getElementById("title").innerText = lang[l].title;
+}
+
+window.onload = applyLang;
